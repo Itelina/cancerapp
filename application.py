6ee9c977dcd31f635e3d935528dd87115c846867
Itelina Ma <itelinama@gmail.com>
2015-08-11 19:28:00 -0400

    modifying application file

--- application.py
+++ application.py
@@ -50,4 +50,5 @@ def score():
 
 # Start the app server on port 80
 # (The default website port)
-app.run(host='0.0.0.0', port=80)
+port = int(os.environ.get("PORT", 33507))
+app.run(host='0.0.0.0', port=port)
